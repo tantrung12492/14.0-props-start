bb1af7bd0df8e898e0ec7bcdab1352eadcfa7880
tantrung1992 <tantrung12492@gmail.com>
2026-04-17 23:46:12 +0700

    fix

--- data.js
+++ data.js
@@ -5,21 +5,25 @@ import pic3 from "./src/assets/pic3.png";
 import pic4 from "./src/assets/pic4.png";
 
 export const myData = [
+  // index 0
   {
     image: pic1,
     title: "componnent",
     desc: "Khối xây dựng giao diện cơ bản - kết hợp nhiều thành phần để tạo nên ứng dụng.",
   },
+  // index 1
   {
     image: pic2,
     title: "JSX",
     desc: "Kết hợp HTML và JavaScript để tạo giao diện động và mạnh mẽ.",
   },
+  // index 2
   {
     image: pic3,
     title: "Props",
     desc: "Truyền dữ liệu vào thành phần để làm nó linh hoạt và tái sử dụng.",
   },
+  // index 3
   {
     image: pic4,
     title: "State",
--- src/component/component1.jsx
+++ src/component/component1.jsx
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-
 function MainContent({ image, title, desc }) {
   return (
     <>
